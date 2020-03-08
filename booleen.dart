3204d9a92732772b7ce41072aba430bcb8c751ce
camara94 <ldamaro98@gmail.com>
2020-03-09 00:05:03 +0100

    Les booléens en langages dart

---
+++ booleen.dart
@@ -0,0 +1,10 @@
+void main() {
+  bool estConnecter = false;
+
+  (estConnecter)? print('Bienvenue sur mon beau Site'):print('Veuillez vous connecter d\'abord');
+
+  estConnecter = true;
+
+  (estConnecter)? print('Bienvenue sur mon beau Site'):print('Veuillez vous connecter d\'abord');
+
+}
