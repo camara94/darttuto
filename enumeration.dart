3407a6565f4ba38d3f0edcb609a9e3ac5c3d747a
camara94 <ldamaro98@gmail.com>
2020-03-09 05:48:25 +0100

    Les énumeration en langage dart

---
+++ enumeration.dart
@@ -0,0 +1,11 @@
+enum Flash {
+  auto,
+  desactive,
+  active
+}
+
+void main() {
+  print('le plash est en mode ${Flash.auto}');
+  print('le plash est en mode ${Flash.desactive}');
+  print('le plash est en mode ${Flash.active}');
+}
