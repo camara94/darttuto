da0c404860f40f3efd055f98cb3dfd3a3c7fced3
camara94 <ldamaro98@gmail.com>
2020-03-09 04:47:37 +0100

    Les classses et hératage en langage dart

--- classes.dart
+++ classes.dart
@@ -1,5 +1,6 @@
 void main() {
   Voiture v1 = new Voiture('Renault', 'Française', 'Jaune', 'Renault 21', 0, 2020);
+  Tracteur t1 = new Tracteur('Tracteur 1', 'Allemande', 'Gris', ' Trac 568', 0, 2026, 50);
 
   print('J\'acheterai une voiture ${v1.nom} de marque ${v1.marque} modèle ${v1.modele} de couleur ${v1.couleur} en ${v1.annee} ');
  
@@ -11,14 +12,36 @@ void main() {
 
   for(int i=0; i < 12; i++) {
     v1.demarer();
+    if(v1.vitesse<=0){
+      print('Votre voiture ${v1.nom} ${v1.couleur} est arrêtée');
+    } else {
+      print('Votre voiture ${v1.nom} ${v1.couleur} roule maintenant à ${v1.vitesse} km/h');
+    }
   }
 
-  if(v1.vitesse<=0){
-    print('Votre voiture ${v1.nom} ${v1.couleur} est arrêtée');
-  } else {
-    print('Votre voiture ${v1.nom} ${v1.couleur} roule maintenant à ${v1.vitesse} km/h');
+  
+
+
+
+  print('J\'acheterai un Tracteur ${t1.nom} de marque ${t1.marque} modèle ${t1.modele} de couleur ${t1.couleur} en ${t1.annee} ');
+ 
+  if(t1.vitesse<=0){
+      print('Votre Tracteur ${t1.nom} ${t1.couleur} est arrêté d\'abord');
+    } else {
+      print('Votre Tracteur ${t1.nom} ${t1.couleur} roule à ${t1.vitesse} km/h');
+    }
+
+  for(int i=0; i < 12; i++) {
+    t1.demarer();
+     if(t1.vitesse<=0){
+        print('Votre Tracteur ${t1.nom} ${t1.couleur} est arrêté');
+      } else {
+        print('Votre Tracteur ${t1.nom} ${t1.couleur} roule maintenant à ${t1.vitesse} km/h');
+      }
+
   }
 
+ 
 }
 
 class Voiture {
@@ -41,4 +64,19 @@ class Voiture {
   void demarer(){
     this.vitesse += 10;
   }
+}
+
+class Tracteur extends Voiture {
+   int vitesseMax;
+   Tracteur(String nom, String marque, String couleur, String modele, int vitesse, int annee, int vitesseMax): super(nom, marque, couleur, modele, vitesse, annee) {
+     this.vitesseMax = vitesseMax;
+   }
+
+  @override
+  void demarer(){
+    super.demarer();
+     if(this.vitesse > this.vitesseMax){
+       this.vitesse = this.vitesseMax;
+     }
+   }
 }
